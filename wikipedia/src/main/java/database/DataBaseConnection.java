c6689c707309098882366ae8b9b00a40a7009eae
Cristina <iaru.cristina99@yahoo.com>
2019-04-20 16:48:40 +0300

    DataBaseConnection

--- wikipedia/src/main/java/database/DataBaseConnection.java
+++ wikipedia/src/main/java/database/DataBaseConnection.java
@@ -1,4 +1,25 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DataBaseConnection {
+
+//		public DatabaseConnection() throws SQLException{}
+//
+
+    public Statement createConn() throws SQLException {
+        String url ="jdbc:mysql://localhost:3306/wikipedia?useSSL=false"; //?useSSL=false - security - optional
+        String user="root";
+        String password="5RrLlQ";
+
+        Connection con= DriverManager.getConnection(url, user, password);
+        Statement st = con.createStatement();
+
+        return st;
+
+    }
+
 }
