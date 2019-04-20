691fdeb3db5c9847c3d5d046775e416d80cbf1ab
BogdanFatu <ionutbogdan11@gmail.com>
2019-04-20 17:40:09 +0300

    Added  JDBC + JPA dependency

--- wikipedia/src/main/java/database/DataBaseConnection.java
+++ wikipedia/src/main/java/database/DataBaseConnection.java
@@ -7,23 +7,19 @@ import java.sql.Statement;
 
 public class DataBaseConnection {
 
-//		public DatabaseConnection() throws SQLException{}
-//
-
 
     /* NU UITATI SA MODIFICATI CONEXIUNEA IN FUNCTIE DEBAZA VOASTRA DE DATE*/
 
     public Statement createConn() throws SQLException {
-        String url ="jdbc:mysql://localhost:3306/wikipedia?useSSL=false"; //?useSSL=false - security - optional
+        String url ="jdbc:mysql://localhost:3306/wiki"; //?useSSL=false - security - optional
         String user="root";
-        String password="5RrLlQ";
-        //String password="1234";
+        //String password="5RrLlQ";
+        String password="1234";
 
         Connection con= DriverManager.getConnection(url, user, password);
         Statement st = con.createStatement();
 
         return st;
-
     }
 
 }
